e6f40df6fdd1a21881abf8cdacafb2cf68df46c9
Gwyneth Margaux Tangog <gwynt@mit.edu>
2026-07-13 13:24:57 -0400

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://yourusername.github.io',
+  base: '/your-repo-name', // set to '/' if using a <username>.github.io repo
+  output: 'static',
+});
