9b62fc07907bbff6bd8a3cfb82c741a41d6fcda9
lyntangog <lyntangog.0831@gmail.com>
2026-07-23 11:43:19 +0800

    base url

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gwyntangog.github.io',
+  site: 'https://lyntangog.github.io',
   base: '/portfolio', // set to '/' if using a <username>.github.io repo
   output: 'static',
 });
